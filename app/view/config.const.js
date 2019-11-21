3a4232af287bfaeb90c10488ee36854bd79e7e8f
marcelo <xreedit@gmail.com>
2019-11-21 11:09:56 -0500

    update socket

--- app/view/config.const.js
+++ app/view/config.const.js
@@ -1,8 +1,8 @@
 var HEADERS_COMPROBANTE = { 'Content-Type': 'application/json', 'Authorization': ''}
 var HEADERS_COMPROBANTE_ONLY_AUTH = { 'Authorization': '' }	
 
-var URL_SOCKET = 'https://app.restobar.papaya.com.pe';
-// var URL_SOCKET = 'http://localhost:5819'
+// var URL_SOCKET = 'https://app.restobar.papaya.com.pe';
+var URL_SOCKET = 'http://localhost:5819'
 // try {
 // 	const HEADERS_COMPROBANTE = { 'Content-Type': 'application/json', 'Authorization': ''}
 // 	const HEADERS_COMPROBANTE_ONLY_AUTH = { 'Authorization': '' }	
