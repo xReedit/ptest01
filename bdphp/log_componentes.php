57cdcb2e4a06970ca94ac1d18eb8b3861183025a
marcelo <xreedit@gmail.com>
2021-11-19 12:36:16 -0500

    direcciones no map

--- bdphp/log_componentes.php
+++ bdphp/log_componentes.php
@@ -63,18 +63,18 @@
         case 7://load clientes
             // $sql="SELECT * FROM cliente where (idorg=$g_ido) AND estado=0 order by nombres";
             // $sql = "select c.* from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres != '' order by nombres";
-            $sql = "select c.idcliente, c.idorg, c.nombres, c.f_nac, c.ruc, c.direccion, c.telefono, c.credito, c.estado, c.pwa_id, c.email, c.calificacion, c.dni_num_verificador from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres != '' order by nombres";
+            $sql = "select c.idcliente, c.idorg, c.nombres, c.f_nac, c.ruc, c.direccion, c.telefono, c.credito, c.estado, c.pwa_id, c.email, c.calificacion, c.dni_num_verificador, c.direccion_delivery_no_map, c.referencia from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres != '' order by nombres";
 			$bd->xConsulta($sql);
             break;
         case 701://load clientes - input autocomplete
             // $sql="SELECT * FROM cliente where (idorg=$g_ido) AND estado=0 order by nombres";
             // $sql = "select c.* from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres != '' order by nombres";
-            $sql = "select c.idcliente, c.idorg, c.nombres, c.f_nac, c.ruc, c.direccion, c.telefono, c.credito, c.estado, c.pwa_id, c.email, c.calificacion, c.dni_num_verificador from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres != '' order by nombres";
+            $sql = "select c.idcliente, c.idorg, c.nombres, c.f_nac, c.ruc, c.direccion, c.telefono, c.credito, c.estado, c.pwa_id, c.email, c.calificacion, c.dni_num_verificador, c.direccion_delivery_no_map, c.referencia from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres != '' order by nombres";
 			$bd->xConsulta($sql);
             break;
         case 70101://load clientes - input autocomplete search val input            
             $inputValue = $_POST['val'];
-            $sql = "select c.idcliente, c.idorg, c.nombres, c.f_nac, c.ruc, c.direccion, c.telefono, c.credito, c.estado, c.pwa_id, c.email, c.calificacion, c.dni_num_verificador from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres like '%$inputValue%' order by nombres";
+            $sql = "select c.idcliente, c.idorg, c.nombres, c.f_nac, c.ruc, c.direccion, c.telefono, c.credito, c.estado, c.pwa_id, c.email, c.calificacion, c.dni_num_verificador, c.direccion_delivery_no_map, c.referencia from cliente_sede cs inner join cliente c on cs.idcliente = c.idcliente where cs.idsede = $g_idsede and nombres like '%$inputValue%' order by nombres";
 			$bd->xConsulta($sql);
             break;
         case 8://load cargos
